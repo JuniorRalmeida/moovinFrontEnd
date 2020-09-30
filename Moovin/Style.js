72082f0aaee4b38a0cd00838b70a2c05ab0b0c78
juniorRalmeida <rubineiralmeida@gmail.com>
2020-09-30 13:55:44 -0300

    Deixando o código mais limpo. Aplicando ifs ternários

--- Moovin/Style.js
+++ Moovin/Style.js
@@ -1,15 +1,8 @@
 function changeColors(campName, onOrOff){
 	if(campName == 1){
-		if(onOrOff == 1){
-			document.getElementById('phone').style.color="green";
-		}else{
+		onOrOff == 1 ? document.getElementById('phone').style.color="green" :
 			document.getElementById('phone').style.color="black";
-		}
 	}else if(campName == 2){
-		if(onOrOff == 1){
-			document.getElementById('whatsApp').style.color="green";
-		}else{
-			document.getElementById('whatsApp').style.color="black";
-		}
-	} 
+		onOrOff == 1 ? document.getElementById('whatsApp').style.color="green" : document.getElementById('whatsApp').style.color="black";
+	}
 }
