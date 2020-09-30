c88d8c87d1c6556aeab6378ff56455bf77593242
juniorRalmeida <rubineiralmeida@gmail.com>
2020-09-30 13:27:21 -0300

    Adicionar primeiro esboço no projeto

---
+++ Moovin/Style.js
@@ -0,0 +1,16 @@
+function changeCollorPhone(value){
+	if(value == 1){
+		document.getElementById('phone').style.color="green";	
+	}else{
+		document.getElementById('phone').style.color="black";
+	}
+}
+
+function changeCollorWhatsApp(value){
+	if(value == 1){
+		document.getElementById('whatsApp').style.color="green";	
+	}else{
+		document.getElementById('whatsApp').style.color="black";
+	}
+}
+
