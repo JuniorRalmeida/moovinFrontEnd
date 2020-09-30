e4f83ab9e7817cf368340e8f518d476e5e195fec
juniorRalmeida <rubineiralmeida@gmail.com>
2020-09-30 13:53:46 -0300

    Deixando o código mais limpo. Deixando tudo em um método só

--- Moovin/Style.js
+++ Moovin/Style.js
@@ -1,16 +1,15 @@
-function changeCollorPhone(value){
-	if(value == 1){
-		document.getElementById('phone').style.color="green";	
-	}else{
-		document.getElementById('phone').style.color="black";
-	}
+function changeColors(campName, onOrOff){
+	if(campName == 1){
+		if(onOrOff == 1){
+			document.getElementById('phone').style.color="green";
+		}else{
+			document.getElementById('phone').style.color="black";
+		}
+	}else if(campName == 2){
+		if(onOrOff == 1){
+			document.getElementById('whatsApp').style.color="green";
+		}else{
+			document.getElementById('whatsApp').style.color="black";
+		}
+	} 
 }
-
-function changeCollorWhatsApp(value){
-	if(value == 1){
-		document.getElementById('whatsApp').style.color="green";	
-	}else{
-		document.getElementById('whatsApp').style.color="black";
-	}
-}
-
